ce311a3e19d433af4173885f6ef2855c200a94ad
automationleg <automationleg@gmail.com>
2023-04-16 23:58:13 +0200

    refactor to flask app working with updated config

--- app.py
+++ app.py
@@ -1,77 +1,46 @@
+from flask import Flask, jsonify
 import cv2
-import numpy as np
-import time
-import logging
-from config.core import config
+from config.core import config, get_class_labels
+from predict import predict_objects, load_yolo_model
 
 
-# Configure logging
-logging.basicConfig(filename='detection.log', level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+app = Flask(__name__)
 
+# Function to be executed before the first request
+# @app.before_first_request
+# def setup():
+#     # Perform any setup logic here
+#     pass
 
-def getOutputsNames(net):
-    # Get the names of all the layers in the network
-    layersNames = net.getLayerNames()
-    n = net.getUnconnectedOutLayers()
-    # Get the indices of the output layers, i.e., the layers that produce the final detections
-    return [layersNames[i-1] for i in net.getUnconnectedOutLayers()]
+@app.route('/detect/<camera_name>')
+def detect(camera_name):
+    # Access the initialized objects in your script logic
+    # load class names
+    # return "200"
+    classes = get_class_labels()
 
-# Load YOLO model
-net = cv2.dnn.readNetFromDarknet(config.model_config.yolo_config_file, config.model_config.yolo_weights_file)
-net.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
-net.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
+    # get YOLO model
+    net = load_yolo_model()
 
-# Load class labels
-with open(config.model_config.coco_names_file, "r") as f:
-    classes = [line.strip() for line in f.readlines()]
+    cam_urls = [cam.url
+               for cam in config.cam_config.cameras 
+               if cam.name == camera_name
+               ]
+    if cam_urls:
+        cam_url = cam_urls[0]
 
-# Open video stream
-cap = cv2.VideoCapture(config.cam_config.cam_schody)  # Replace with your video URL or video file path
+    # Open video stream
+    cap = cv2.VideoCapture(cam_url)  # Replace with your video URL or video file path
 
-def predict_objects(cap: cv2.VideoCapture) -> dict:
-    ret, frame = cap.read()
+    detections = predict_objects(cap=cap,net=net, classes=classes)
+    print(detections)
+    
+    # Release video capture and close all OpenCV windows
+    cap.release()
+    # cv2.destroyAllWindows()
 
-    if ret:
-        # Get current time
-        current_time = time.time()
+    # Return the result in JSON format
+    return jsonify(detections['object'])
 
-        # Convert frame to blob
-        blob = cv2.dnn.blobFromImage(frame, 1/255.0, (416, 416), swapRB=True, crop=False)
-
-        # Set the input blob to the network
-        net.setInput(blob)
-
-        # Forward pass and get the output from the output layers
-        outs = net.forward(getOutputsNames(net))
-
-        # Process each output layer
-        objects_detected = {}
-        for out in outs:
-            for detection in out:
-                scores = detection[5:]
-                classId = np.argmax(scores)
-                confidence = scores[classId]
-
-                # Filter out weak detections
-                if confidence > 0.5:
-                    # Get bounding box coordinates
-                    x, y, w, h = detection[:4] * np.array([frame.shape[1], frame.shape[0], frame.shape[1], frame.shape[0]])
-                    x, y, w, h = int(x), int(y), int(w), int(h)
-
-                    # Draw bounding box and label on the frame
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                    cv2.putText(frame, classes[classId], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-
-                    logging.info('object="{}" detected at x={}, y={}, width={}, height={}, confidence={}, current_time={:.2f}s'.format(classes[classId],x, y, w, h, confidence, current_time))
-                    objects_detected["object"] = classes[classId]
-                    objects_detected["confidence"] = confidence
-        
-        return objects_detected
-
-detections = predict_objects(cap=cap)
-print(detections)
-
-# Release video capture and close all OpenCV windows
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    app.run(debug=True)
--- config/core.py
+++ config/core.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
 
 from pydantic import BaseModel
 from strictyaml import YAML, load
@@ -8,13 +8,12 @@ from strictyaml import YAML, load
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 CONFIG_FILE_PATH = PROJECT_ROOT / "config.yml"
 
-class Cameras(BaseModel):
-    # cam_garaz: str
-    # cam_taras: str
-    # cam_wejscie: str
-    # cam_maly_taras: str
-    cam_schody: str
-    cam_salon: str
+class Camera(BaseModel):
+    name: str
+    url: str
+
+class CamConfig(BaseModel):
+    cameras: List[Camera]
 
 class ModelConfig(BaseModel):
     yolo_config_file: str
@@ -23,7 +22,7 @@ class ModelConfig(BaseModel):
 
 class Config(BaseModel):
     model_config: ModelConfig
-    cam_config: Cameras
+    cam_config: CamConfig
 
 
 def find_config_file() -> Path:
@@ -54,7 +53,7 @@ def create_and_validate_config(parsed_config: YAML = None) -> Config:
 
     # specify the data attribute from the strictaml YAML type
     _config = Config(
-        cam_config=Cameras(**parsed_config.data),
+        cam_config=CamConfig(**parsed_config.data),
         model_config=ModelConfig(**parsed_config.data)
     )
     return _config
